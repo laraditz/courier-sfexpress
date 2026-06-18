57b7d005486786de14f451d036acb3569eed2f7d
Raditz Farhan <raditzfarhan@gmail.com>
2026-06-18 15:00:05 +0800

    feat: add SfExpressServiceProvider with driver registration and config merge

--- src/SfExpressServiceProvider.php
+++ src/SfExpressServiceProvider.php
@@ -6,7 +6,22 @@ use Illuminate\Support\ServiceProvider;
 
 class SfExpressServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/sfexpress.php', 'courier.drivers.sfexpress');
+    }
 
-    public function boot(): void {}
+    public function boot(): void
+    {
+        $this->app->make('courier')->extend(
+            'sfexpress',
+            fn ($app, $config) => new SfExpressDriver($config)
+        );
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/sfexpress.php' => config_path('courier.php'),
+            ], 'courier-sfexpress-config');
+        }
+    }
 }
